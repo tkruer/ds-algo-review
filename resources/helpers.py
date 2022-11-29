9e12bfbb51dcc477f5eba3d8ae385f4d9b5a5c03
Tyler K <tylerkruer1@gmail.com>
2022-11-29 10:38:13 -0700

    added BS

--- resources/helpers.py
+++ resources/helpers.py
@@ -4,32 +4,8 @@ from src import binarysearch
 def display_options():
     print('Choose from the following actions: ')
     action_menu = '''
-    Root (R)
-    Retrieve account details (RAD)
-    Create account funding source (CAFS)
-    Create account VAN (CAVAN)
-    List account funding sources (LAFS)
-    List and search account transfers (LASAT)
-    List account mass payments (LAMP)
-    Create receive only customer (CROC)
-    Create unverified customer (CUVC)
-    Create verified personal customer (CVP)
-    Retrieve customer (RC)
-    List and search customers (LASC)
-    Update customer (UC)
-    List customer business classifications (LCBC)
-    retrieve business classification (RBC)
-    initiate KBA (IKBA)
-    retrieve KBA (RKBA)
-    verify KBA (VKBA)
-    Create beneficial owner (CBO)
-    Retrieve beneficial owner (RBO)
-    list beneficial owners (LBO)
-    update beneficial owner (UBO)
-    Remove beneficial owner (DBO)
-    Retrieve beneficial ownership status (RBOS)
-    Certify beneficial ownership (CBOS)
-    Quit (Q)
+    Binary Search (BS)
+    Exit (Q)
     '''
     print(action_menu)
 
@@ -38,10 +14,10 @@ def get_user_input():
 
 def handle_input(input):
 
-    if input == 'recBinarySearchArray':
-        recBinarySearchArray = [1, 2, 3, 4, 5]
-        recBinarySearchTarget = 3
-        src.binarysearch.recursivebinarysearch(recBinarySearchArray, recBinarySearchTarget)
+    if input == 'BS':
+        ibs_array = [1, 2, 3, 4, 5]
+        ibs_array = 3
+        src.binarysearch.iteractivebinarysearch(array=ibs_array, target=ibs_array)
 
     elif input == 'Q':
         quit()
