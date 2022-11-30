8f3f8d1bc35783afa7e5f74a0a3829be850af9f1
Tyler K <tylerkruer1@gmail.com>
2022-11-30 16:17:18 -0700

    commit for now

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 from resources import helpers
 
-
-
 def main():
-    exit = False
-    while not exit:
-        helpers.display_options()
-        input = helpers.get_user_input()
-        if input == 'exit':
-            exit = True
-        else:
-            helpers.handle_input(input)
-
-
+    helpers.display_options()
+    input = helpers.get_user_input()
+    if input == 'exit':
+        exit()
+    else:
+        helpers.handle_input(input)
 
 if __name__ == '__main__':
     main()
-
 
 
--- resources/helpers.py
+++ resources/helpers.py
@@ -1,23 +1,25 @@
-import src.binarysearch
-from src import binarysearch
+import src.binarysearch, src.linkedlist
+import time
+
 
 def display_options():
     print('Choose from the following actions: ')
     action_menu = '''
     Binary Search (BS)
-    Exit (Q)
+    Linear Search (LS)
+    Merge Sort (MS)
+    Bubble Sort (BBS)
+    
+    Exit (exit)
     '''
     print(action_menu)
 
 def get_user_input():
     return input('Enter your action: ')
-
+1
 def handle_input(input):
 
     if input == 'BS':
-        ibs_array = [1, 2, 3, 4, 5]
-        ibs_target = 3
-        print(src.binarysearch.iteractivebinarysearch(array=ibs_array, target=ibs_target))
-
-    elif input == 'Q':
-        quit()
+        start_time = time.time()
+        print(src.binarysearch.iteractivebinarysearch(array=[1, 2, 3, 4, 5], target=3), (time.time() - start_time))
+    elif input == 'LS':
